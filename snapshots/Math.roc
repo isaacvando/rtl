06b155e5f01f4bc808ad60bd5574164a0b5fb3ad
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2024-12-03 21:03:23 -0600

    Add snapshot testing (#18)
    * wip
    
    * wip
    
    * basic snapshot testing
    
    * run snapshot tests in ci
    
    * fix command
    
    * add message to script
    
    * update template (without updating snapshot)
    
    * use git diff instead
    
    * add the ability to automatically update snapshots
    
    * add more features to the test
    
    * switch script to bash

---
+++ snapshots/Math.roc
@@ -0,0 +1,4 @@
+module [add]
+
+add = \x, y ->
+    x + y
---
+++ snapshots/test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+roc build ../rtl.roc
+../rtl
+roc output.roc > output.txt
+
+if [ "${1:-}" = "-u" ]; then
+    cp output.txt expectedOutput.txt
+    cp Pages.roc ExpectedPages.roc
+    echo "Updated snapshots 🎉"
+    exit 0
+fi
+
+exit_code=0
+if ! git diff --no-index "output.txt" "expectedOutput.txt"; then
+    exit_code=1
+fi
+
+if ! git diff --no-index "Pages.roc" "ExpectedPages.roc"; then
+    exit_code=1
+fi
+
+if [ $exit_code -eq 0 ]; then
+    echo "All snapshots passed ✨"
+else
+    echo "Some snapshots failed ✋"
+    echo 'If all of the changes look correct, you can update the expected values by running `./test.sh -u` and committing the changes.'
+fi
+
+exit $exit_code
